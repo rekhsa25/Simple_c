0b747e05b2db4cb6b5fa66b9ab60782d35e593b1
root <root@ip-172-31-16-202.us-east-2.compute.internal>
2019-02-14 15:06:09 +0000

    changes are done

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include<stdio.io>
+#include<stdio.h>
 void main()
 {
 	printf("My name is Rekha\n");
-	printf("I am learning Devops");
+	printf("I am learning Devops\n");
 }
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 ABC.exe:main.o
-        gcc -o ABC.exe main.o
+	gcc -o ABC.exe main.o
 main.o:main.c
-        gcc -c main.c
+	gcc -c main.c
