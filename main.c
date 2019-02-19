092175bc8af829583ca58fa2ea42b70335624294
rekhsa25 <47451502+rekhsa25@users.noreply.github.com>
2019-02-20 00:47:13 +0530

    updated main.c in feature1 branch

--- main.c
+++ main.c
@@ -4,4 +4,5 @@ void main()
 	printf("My name is Rekha\n");
 	printf("I am learning Devops\n");
 	printf(" adding new line into this main program to check auto trigger function");
+	printf("this line iadded by feature1 branch");
 }
